d0bd56a873e12be635d5c6c1da7d95c43263cd34
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-05-16 16:43:28 +0100

    restaurant reviews average ratings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0208 949 1115',
+    category: 'Thai'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '0208 949 1111',
+    category: 'Pizza'
+  },
+  {
+    name:         'Pizza West',
+    address:      '56A ShoreBitch High St, London E1 6PQ',
+    phone_number:  '0208 949 2311',
+    category: 'Pizza'
+  },
+  {
+    name:         'Curry House',
+    address:      '56 Curry High St, London E1 6PQ',
+    phone_number:  '0207 829 1111',
+    category: 'Curry'
+  },
+  {
+    name:         'Chinese Cuisine',
+    address:      '82A Chinese High St, London E8 6PQ',
+    phone_number:  '0208 946 6666',
+    category: 'Chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
